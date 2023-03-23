68fb8d7907775715c561495d09864205aa54c02a
TheBlaze <santi.volcanos@gmail.com>
2023-03-23 10:43:14 -0600

    first commit

---
+++ Exam_Santiago.cpp
@@ -0,0 +1,13 @@
+// Exam_Santiago.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
